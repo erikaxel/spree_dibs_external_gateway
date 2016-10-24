99acd98037b1e13d97e6b61e66187fa81b73540b
Erik Axel Nielsen <erikaxel@nielsen-og-co.no>
2016-10-24 19:03:52 +0200

    Spree::Core::Engine.add_routes => Spree::Core::Engine.routes.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.add_routes do
+Spree::Core::Engine.routes.draw do
   post '/dibs/return', to: 'checkout#dibs_return'
   post '/dibs/cancel', to: 'checkout#dibs_cancel'
   post '/dibs/test_post', to: 'dibs#test_post'
